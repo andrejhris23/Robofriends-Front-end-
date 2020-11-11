383d468f1c950f5bc7a22c411d3d9ff4578f42aa
Andrej Hristov <andrejhris23@gmail.com>
2020-11-11 16:02:05 +0100

    finished with this mini app

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({serachfield, searchChange}) => {
     return (
         <div className='p2'>
             <input
-                className='pa3 ba b--green bg-lightest-blue'
+                className='pa3 ba b--green bg-lightest-blue mb3'
                 type='search'
                 placeholder='search robots'
                 onChange={searchChange}
